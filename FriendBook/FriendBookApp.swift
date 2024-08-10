aec8993e0c165b561a1b88e19df5c940599b21b5
Adam Sayer <asayer82@icloud.com>
2024-08-11 06:40:50 +1000

    Initial Commit

---
+++ FriendBook/FriendBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendBookApp.swift
+//  FriendBook
+//
+//  Created by Adam Sayer on 11/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
