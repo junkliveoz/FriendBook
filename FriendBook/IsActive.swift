04298d62d1668180e84fb64c6782ea71e62bb376
Adam Sayer <asayer82@icloud.com>
2024-08-12 21:37:09 +1000

    Completed day 61 activity to migrate app to SwiftData

---
+++ FriendBook/IsActive.swift
@@ -0,0 +1,18 @@
+//
+//  IsActive.swift
+//  FriendBook
+//
+//  Created by Adam Sayer on 12/8/2024.
+//
+
+import SwiftUI
+
+struct IsActive: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IsActive()
+}
---
+++ FriendBook/UserDetail.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetail.swift
+//  FriendBook
+//
+//  Created by Adam Sayer on 12/8/2024.
+//
+
+import SwiftUI
+
+struct UserDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetail()
+}
